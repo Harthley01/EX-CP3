569236c3814def1a46722ecee7b2f90ffee29d15
Harthley01 <rm551359@fiap.com.br>
2023-09-13 12:20:25 -0300

    arrumando

--- ex-cp5/src/main.jsx
+++ ex-cp5/src/main.jsx
@@ -1,7 +1,8 @@
 import App from './App.jsx';
+import App from './App.css';
 import React from 'react'; 
 import ReactDOM from 'react-dom/client'; 
-import './routes/Produtos/index.css'
+import './routes/Produtos/index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home/index.jsx';
 import Produtos from './routes/Produtos/index.jsx';
